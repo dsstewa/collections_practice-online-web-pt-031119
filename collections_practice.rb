ecf35de638f9f07f717ec13d62ecb41960490d2d
David Stewart <davidstewart@gmail.com>
2019-03-22 03:04:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,7 @@ def kesha_maker(array)
  new_array = []
  array.each_with_index do |names,index|
    new_array[index] << names[0..1]
-   new_array[index] << $
+   new_array[index] << "$"
    new_array[index] << names[3..]
    
    binding.pry
